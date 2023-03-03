3ae8cc277dacc000a57d9814041f40a18ae2649e
hansson0950 <emil.hansson@live.se>
2023-03-03 10:48:58 +0100

    Added a route for CreateAccountScreen. Added a route in NavHost, Router, and a template in CreateAccountScreen. This will enable the user to navigate to the sign up screen from the login screen.

--- HomeApp/app/src/main/java/com/HomeApp/screens/CreateAccountScreen.kt
+++ HomeApp/app/src/main/java/com/HomeApp/screens/CreateAccountScreen.kt
@@ -1,2 +1,13 @@
 package com.HomeApp.screens
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun CreateAccountScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    OnSelfClick: () -> Unit = {}
+) {
+}
