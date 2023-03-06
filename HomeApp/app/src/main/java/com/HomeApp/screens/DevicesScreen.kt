b42499e897a4d4b26fcfeb2c55baebd10903ba0f
RammeDon <raminkhareji@gmail.com>
2023-03-06 16:50:37 +0100

    Added row of buttons for selecting all or favourites

--- HomeApp/app/src/main/java/com/HomeApp/screens/DevicesScreen.kt
+++ HomeApp/app/src/main/java/com/HomeApp/screens/DevicesScreen.kt
@@ -1,14 +1,16 @@
 package com.HomeApp.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.HomeApp.ui.composables.TitleBar
 import com.HomeApp.ui.navigation.NavPath
@@ -26,7 +28,7 @@ fun DevicesScreen(
         },
         content = {
             Column(modifier = Modifier.padding(it)) {
-                
+
             }
         },
         bottomBar = {
--- HomeApp/app/src/main/java/com/HomeApp/ui/composables/TitleBar.kt
+++ HomeApp/app/src/main/java/com/HomeApp/ui/composables/TitleBar.kt
@@ -1,20 +1,23 @@
 package com.HomeApp.ui.composables
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import kotlin.math.round
 
 
 @Composable
@@ -26,28 +29,72 @@ fun TitleBar(
     navController: NavController
 
 ) {
-    Row(
-        modifier = Modifier
-            .height(60.dp)
-            .padding(horizontal = 20.dp)) {
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(
-                imageVector = Icons.Filled.ArrowBack,
-                contentDescription = "arrow-back",
-                modifier = Modifier.size(50.dp)
-                    .padding(top = 10.dp))
+    val selectedColor = 0xFFAECCE4
+    val notSelectedColor = 0xFFC5C5C5
+
+    var leftSelected by remember { mutableStateOf(true) }
+
+    Column(modifier = Modifier) {
+        Row(
+            modifier = Modifier
+                .height(60.dp)
+                .padding(horizontal = 20.dp)) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "arrow-back",
+                    modifier = Modifier
+                        .size(50.dp)
+                        .padding(top = 10.dp))
+            }
+            Text(text = screenTitle,
+                modifier = Modifier.weight(2f),
+                fontSize = 40.sp,
+                textAlign = TextAlign.Center)
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Outlined.Settings,
+                    contentDescription = "settings-icon",
+                    modifier = Modifier
+                        .size(50.dp)
+                        .padding(top = 10.dp))
+            }
         }
-        Text(text = screenTitle,
-            modifier = Modifier.weight(2f),
-            fontSize = 40.sp,
-            textAlign = TextAlign.Center)
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(imageVector = Icons.Outlined.Settings,
-                contentDescription = "settings-icon",
-                modifier = Modifier
-                    .size(50.dp)
-                    .padding(top = 10.dp))
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .height(20.dp))
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)) {
+            Button(onClick = { /*TODO*/ }, modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+                colors = ButtonDefaults.buttonColors(backgroundColor = if (leftSelected) Color(selectedColor) else Color(notSelectedColor)
+                ),
+                shape = RoundedCornerShape(0.dp)
+
+            ) {
+                Column(modifier = Modifier
+                    .fillMaxSize()) {
+                    Text(text = "All", textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
+                    Text(text = "12", textAlign = TextAlign.Center, color = Color.DarkGray, modifier = Modifier.fillMaxWidth())
+                }
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+                colors = ButtonDefaults.buttonColors(if (leftSelected) Color(notSelectedColor) else Color(selectedColor)),
+                shape = RoundedCornerShape(0.dp)
+            ) {
+                Column(modifier = Modifier
+                    .fillMaxSize()) {
+                    Text(text = "Favourites", textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
+                    Text(text = "3", textAlign = TextAlign.Center, color = Color.DarkGray, modifier = Modifier.fillMaxWidth())
+                }
+            }
         }
+
     }
+
+
 
 }
