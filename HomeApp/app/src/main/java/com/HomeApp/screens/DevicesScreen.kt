4d8e078506b8745eab44287354701f783205afbe
RammeDon <raminkhareji@gmail.com>
2023-03-06 14:28:31 +0100

    Creating TitleBar

--- HomeApp/app/src/main/java/com/HomeApp/screens/DevicesScreen.kt
+++ HomeApp/app/src/main/java/com/HomeApp/screens/DevicesScreen.kt
@@ -1,10 +1,16 @@
 package com.HomeApp.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.HomeApp.ui.composables.TitleBar
+import com.HomeApp.ui.navigation.NavPath
 
 @Composable
 fun DevicesScreen(
@@ -14,10 +20,12 @@ fun DevicesScreen(
 ) {
     Scaffold(
         topBar = {
-
+            TitleBar(screenTitle = "Devices", previousScreen = navController.currentBackStackEntry?.destination?.route?)
         },
         content = {
-
+            Column(modifier = Modifier.padding(it)) {
+                
+            }
         },
         bottomBar = {
 
--- HomeApp/app/src/main/java/com/HomeApp/ui/composables/TitleBar.kt
+++ HomeApp/app/src/main/java/com/HomeApp/ui/composables/TitleBar.kt
@@ -1,13 +1,14 @@
 package com.HomeApp.ui.composables
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 
 @Composable
@@ -17,10 +18,14 @@ fun TitleBar(
     previousScreen: String, // This is to know what screen to navigate to when pressing back
 
     modifier: Modifier = Modifier,
-    navigation: Navigation
+    navController: NavController
 
 ) {
     Row(modifier = Modifier.height(30.dp)) {
+        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "arrow-back")
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = screenTitle, modifier = Modifier.weight(2f))
+        Spacer(modifier = Modifier.weight(1f))
 
 
     }
