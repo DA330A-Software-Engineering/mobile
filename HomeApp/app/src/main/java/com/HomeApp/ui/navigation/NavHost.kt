5272284a3d202943db6258ce7b93277266879d12
sam-hur <83226062+sam-hur@users.noreply.github.com>
2023-03-06 16:19:26 +0100

    Updated MainActivity, SideBar, NavHost and Values

--- HomeApp/app/src/main/java/com/HomeApp/ui/navigation/NavHost.kt
+++ HomeApp/app/src/main/java/com/HomeApp/ui/navigation/NavHost.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.material.ScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -19,7 +20,8 @@ import com.google.accompanist.navigation.animation.composable
 @Composable
 fun AnimatedAppNavHost(
     modifier: Modifier = Modifier, navController: NavHostController,
-    startDestination: String
+    startDestination: String,
+    state: ScaffoldState
 ) {
     // Calls the navigate function to control movement between views/screens in the app
     val defaultTween = 450
@@ -53,7 +55,23 @@ fun AnimatedAppNavHost(
         ) {
             HomeScreen(
                 navController = navController,
-                OnSelfClick = { navController.navigateSingleTopTo(Login.route) }
+                OnSelfClick = { navController.navigateSingleTopTo(Login.route) },
+                state = state
+            )
+        }
+
+        // DEVICES
+        composable(
+            route = Devices.route,
+            enterTransition = { fadeIn(tween(defaultTween)) },
+            popEnterTransition = { fadeIn(tween(defaultTween)) },
+            exitTransition = { fadeOut(tween(defaultTween)) },
+            popExitTransition = { fadeOut(tween(defaultTween)) }
+        ) {
+            DevicesScreen(
+                navController = navController,
+                OnSelfClick = { navController.navigateSingleTopTo(ConfirmToken.route) },
+                state = state
             )
         }
 
--- HomeApp/app/src/main/java/com/HomeApp/util/Values.kt
+++ HomeApp/app/src/main/java/com/HomeApp/util/Values.kt
@@ -1,4 +1,18 @@
 package com.HomeApp.util
 
-class Values {
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Mic
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.icons.rounded.People
+import androidx.compose.ui.graphics.vector.ImageVector
+
+val microphoneIcon: ImageVector = Icons.Rounded.Mic
+
+enum class SideBarOptions(val title: String, val icon: ImageVector?, val route: String) {
+    PROFILE("Profile & Family", Icons.Rounded.People, ""),
+    NOTIFICATIONS("Notifications", Icons.Rounded.Notifications, ""),
+    History("History", null, ""),
+    LOGOUT("Logout", null, ""),
 }
+
+val enableTopDrawer: Boolean = false
