dc5aafe5dcf5ecbcad351208cd4b99f9a1e609df
RammeDon <raminkhareji@gmail.com>
2023-03-09 21:50:02 +0100

    Adding interface and classes to create dummy data to work on screen

--- HomeApp/app/src/main/java/com/HomeApp/screens/DevicesScreen.kt
+++ HomeApp/app/src/main/java/com/HomeApp/screens/DevicesScreen.kt
@@ -2,6 +2,7 @@ package com.HomeApp.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -17,14 +18,22 @@ import com.HomeApp.ui.composables.TitleBar
 import com.HomeApp.ui.composables.TitledDivider
 import com.HomeApp.ui.navigation.NavPath
 import com.HomeApp.util.DevicesFilters
+import org.json.JSONObject
 import java.util.Objects
 
 sealed interface DEVICES{
     var id: String
     var type: String
-    var state: Objects
+    var state: JSONObject
+    var name: String
 }
 
+class DevicesDummy: DEVICES{
+    override var id: String = ""
+    override var type: String= ""
+    override var name: String = ""
+    override var state: JSONObject = JSONObject()
+}
 
 
 
@@ -37,6 +46,27 @@ fun DevicesScreen(
 ) {
     //var filtersSelected by remember { mutab }
 
+    val device1 = DevicesDummy()
+    device1.id = "11"
+    device1.type = "light"
+    device1.name = "Living Room Light"
+    device1.state = JSONObject("{'name': 'hi'}")
+    val device2 = DevicesDummy()
+    device1.id = "11"
+    device1.type = "light"
+    device1.name = "Living Room Light"
+    device1.state = JSONObject("{'name': 'hi'}")
+    val device3 = DevicesDummy()
+    device1.id = "11"
+    device1.type = "light"
+    device1.name = "Living Room Light"
+    device1.state = JSONObject("{'name': 'hi'}")
+
+    val devicesList = remember {
+        mutableListOf(device1, device2, device3)
+    }
+
+
     Scaffold(
         topBar = {
 
@@ -48,6 +78,14 @@ fun DevicesScreen(
                 .height(200.dp)) {
                 TitledDivider(navController = navController, title = "Filters")
                 FilteredList(filterScreen="devices")
+
+                /*LazyColumn(content = {
+                    devicesList.forEach({
+                        item {
+
+                        }
+                    })
+                })*/
 
             }
         },
