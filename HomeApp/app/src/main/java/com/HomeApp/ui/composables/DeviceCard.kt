8af24e8e83f28551c8d2a9de8accaefad1598867
RammeDon <raminkhareji@gmail.com>
2023-03-09 22:15:07 +0100

    Moving DeviceFilters enum class into values file

--- HomeApp/app/src/main/java/com/HomeApp/ui/composables/DeviceCard.kt
+++ HomeApp/app/src/main/java/com/HomeApp/ui/composables/DeviceCard.kt
@@ -1,19 +1,34 @@
 package com.HomeApp.ui.composables
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
 import androidx.navigation.NavController
+import com.HomeApp.R
+import com.HomeApp.screens.DevicesDummy
 
 @Composable
 fun DeviceCard(
     navController: NavController,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    deviceItem: DevicesDummy
 ) {
 
-    Box(modifier = Modifier.fillMaxWidth()) {
+    val cardIcon: ImageVector = when (deviceItem.type){
+        is "light" ->
+    }
 
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .background(colorResource(id = R.color.LightSteelBlue))) {
+
+        Icon(imageVector = if (), contentDescription ="")
     }
 
 }
