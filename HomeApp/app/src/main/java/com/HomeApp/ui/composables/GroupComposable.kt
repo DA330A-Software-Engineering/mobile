483c5b505e110b971fde609c739160473e842c35
RammeDon <raminkhareji@gmail.com>
2023-04-05 20:28:32 +0200

    Creating groups data class

--- HomeApp/app/src/main/java/com/HomeApp/ui/composables/GroupComposable.kt
+++ HomeApp/app/src/main/java/com/HomeApp/ui/composables/GroupComposable.kt
@@ -1,13 +1,32 @@
 package com.HomeApp.ui.composables
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.Navigation
+
+data class Groups(
+    var id: String = "",
+    var name: String = "",
+    var description: String = "",
+    var state: Boolean = false,
+    var available: Boolean = true
+)
 
 @Composable
 fun GroupComposable(
-    navigation: Navigation = Navigation,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    groupName: String,
+    groupState: String
 ) {
-    
+    Column(modifier = Modifier
+        .width(20.dp)
+        .height(20.dp)
+    ) {
+        Text(text = groupName, modifier = Modifier.padding(horizontal = 3.dp))
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(text = groupState, modifier = Modifier.padding(horizontal = 3.dp))
+    }
 }
