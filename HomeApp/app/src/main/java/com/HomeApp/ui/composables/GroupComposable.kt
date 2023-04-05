28928272bf01eb1b05fc4d27a7f1a2a990ae6786
RammeDon <raminkhareji@gmail.com>
2023-04-05 20:11:52 +0200

    Creating GroupComposable

---
+++ HomeApp/app/src/main/java/com/HomeApp/ui/composables/GroupComposable.kt
@@ -0,0 +1,13 @@
+package com.HomeApp.ui.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.Navigation
+
+@Composable
+fun GroupComposable(
+    navigation: Navigation = Navigation,
+    modifier: Modifier = Modifier
+) {
+    
+}
