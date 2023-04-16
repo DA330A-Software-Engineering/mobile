c0c54237c2f487859a68c4dab747e413a014a308
RammeDon <raminkhareji@gmail.com>
2023-04-16 04:48:24 +0200

    adding fields to dialog

--- HomeApp/app/src/main/java/com/HomeApp/ui/composables/GroupComposable.kt
+++ HomeApp/app/src/main/java/com/HomeApp/ui/composables/GroupComposable.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
@@ -77,7 +79,7 @@ fun GroupComposable(
             AlertDialog(
                 onDismissRequest = { editGroup = false },
                 title = { Text(groupItem.get("name") as String) },
-                text = { editGroup()},
+                text = { editGroup(groupItem = groupItem)},
                 confirmButton = {
                     Button(
                         onClick = { editGroup = false },
@@ -93,9 +95,38 @@ fun GroupComposable(
 
 @Composable
 private fun editGroup(
-    modifier: Modifier= Modifier
+    modifier: Modifier= Modifier,
+    groupItem: DocumentSnapshot,
 ) {
-    Column(modifier = Modifier) {
+    var groupName by remember { mutableStateOf(groupItem.get("name") as String) }
+    var groupDesc by remember { mutableStateOf(groupItem.get("description") as String) }
+
+    Column(modifier = Modifier, verticalArrangement = Arrangement.spacedBy(5.dp)) {
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(modifier = Modifier
+            .height(50.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Name", modifier = Modifier.weight(2f))
+            Spacer(modifier = Modifier.weight(0.1f))
+            TextField(value = groupName, onValueChange = {groupName = it}, modifier = Modifier.width(100.dp).weight(5f))
+        }
+        Row(modifier = Modifier
+            .height(50.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Description", modifier = Modifier.weight(2f))
+            Spacer(modifier = Modifier.weight(0.1f))
+            TextField(value = groupDesc, onValueChange = {groupDesc = it}, modifier = Modifier.width(100.dp).weight(5f))
+        }
+        Row(modifier = Modifier
+            .height(50.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Devices", modifier = Modifier.weight(2f))
+            Spacer(modifier = Modifier.weight(0.1f))
+            TextField(value = groupDesc, onValueChange = {groupDesc = it}, modifier = Modifier.width(100.dp).weight(5f))
+        }
+
+
+
 
     }
 }
