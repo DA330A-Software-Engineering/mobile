8f84f9ba714672aac6dc5b9d6c83cdad66b1262d
hansson0950 <emil.hansson@live.se>
2023-04-29 19:45:44 +0200

    Adding routines collection to RealTimeData class

--- HomeApp/app/src/main/java/com/HomeApp/util/FireStoreAccess.kt
+++ HomeApp/app/src/main/java/com/HomeApp/util/FireStoreAccess.kt
@@ -1,21 +1,10 @@
 package com.HomeApp.util
 
 import android.content.Context
-import android.util.Log
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.ui.platform.LocalContext
-import com.HomeApp.screens.Devices
-import com.HomeApp.ui.composables.GroupsClass
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.json.JSONObject
-import kotlin.coroutines.coroutineContext
 
 fun rememberFirestoreCollection(
     collectionPath: String,
@@ -26,7 +15,7 @@ fun rememberFirestoreCollection(
     val documents = mutableStateListOf<DocumentSnapshot>()  //mutableStateOf(MutableList<T>())
     if (email != "") {
         val collectionRef =
-            if (collectionType == "groups" || collectionType == "routine") FirebaseFirestore.getInstance()
+            if (collectionType == "groups" || collectionType == "routines") FirebaseFirestore.getInstance()
                 .collection("profiles").document(email).collection(collectionType)
             else FirebaseFirestore.getInstance().collection(collectionPath)
         collectionRef.addSnapshotListener { snapshot, error ->
@@ -73,6 +62,19 @@ fun getDocument(collectionPath: String, documentPath: String, callback: (documen
 
 class RealTimeData(context: Context) {
     var email = getEmailFromToken(context)
-    val devices = rememberFirestoreCollection(collectionPath = "devices",  collectionType = "devices", userEmail = email)
-    val groups = rememberFirestoreCollection(collectionPath = "", collectionType = "groups", userEmail = email)
+    val devices = rememberFirestoreCollection(
+        collectionPath = "devices",
+        collectionType = "devices",
+        userEmail = email
+    )
+    val groups = rememberFirestoreCollection(
+        collectionPath = "",
+        collectionType = "groups",
+        userEmail = email
+    )
+    val routines = rememberFirestoreCollection(
+        collectionPath = "",
+        collectionType = "routines",
+        userEmail = email
+    )
 }
